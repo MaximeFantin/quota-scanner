0492f5933aabe499970abd9386b3f7458b2b503b
MaximeFantin <180168842+MaximeFantin@users.noreply.github.com>
2026-01-26 18:29:49 +0100

    Add files via upload

---
+++ scan-quota.py
@@ -0,0 +1,68 @@
+import os
+
+def humanReadable(size):
+    if size > 1_000_000_000:
+        return f"{size/1024**3:.2f}G"
+    if size > 1_000_000:
+        return f"{size/1024**2:.2f}M"
+    if size > 1_000:
+        return f"{size/1024:.2f}K"
+
+
+directory = '/home/iut45/Etudiants/o22407574/'
+
+def scan():
+    explored = 0
+    folders = {}
+    for root, _, files in os.walk(directory):
+        for filename in files:
+            file = os.path.join(root, filename)
+            if os.path.isfile(file):
+                path = ""
+                for folder in file.split("/")[4:-1]:
+                    folders[path + folder] = folders.get(path + folder, 0) + os.path.getsize(file)
+                    path += folder + "/"
+            explored += 1
+            if not explored & 0b0001_1111_1111:
+                print(f"{explored} files scanned")
+
+    print("\n--- 10 biggest folders ---")
+
+    flist = list(folders.items())
+    flist = sorted(flist, key=lambda x: x[1])[::-1]
+    stdo = ""
+    for folder, size in flist[:10]:
+        stdo += f"\033[94m{folder}\033[0m {humanReadable(size)}\n"
+    print(stdo)
+
+
+    fout = []
+    for fo, si in flist:
+        fout.append(fo)
+        fout.append(" ")
+        fout.append(str(si))
+        fout.append("\n")
+
+    with open("scan.dat", "w") as f:
+        f.write("".join(fout))
+
+
+def topSearch(amount):
+    with open("scan.dat", "r") as file:
+        lines = []
+        for i in range(amount):
+            line = file.readline()
+            if not line:
+                continue
+            line = line.split()
+            folder = "".join(line[:-1])
+            size = int(line[-1])
+            lines.append((folder, size))
+    
+    print(f"\n--- {amount} biggest folders ---")
+    stdo = ""
+    for folder, size in lines:
+        stdo += f"\033[94m{folder}\033[0m {humanReadable(size)}\n"
+    print(stdo)
+
+topSearch(50)
